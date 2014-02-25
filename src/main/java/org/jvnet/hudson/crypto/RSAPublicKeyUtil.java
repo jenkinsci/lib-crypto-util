1acb493e8cd7ff1faa1399f0114968b53b406eb2
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-25 11:56:41 -0800

    added fingerprint extraction code

--- src/main/java/org/jvnet/hudson/crypto/RSAPublicKeyUtil.java
+++ src/main/java/org/jvnet/hudson/crypto/RSAPublicKeyUtil.java
@@ -1,15 +1,22 @@
 package org.jvnet.hudson.crypto;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
+import java.security.MessageDigest;
 import java.security.PublicKey;
+import java.security.interfaces.DSAPublicKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.DSAPublicKeySpec;
 import java.security.spec.RSAPublicKeySpec;
 
@@ -73,4 +80,79 @@ public class RSAPublicKeyUtil {
             return new BigInteger(buf);
         }
     }
+
+    static class DataOutputStream2 extends DataOutputStream {
+        DataOutputStream2(OutputStream out) {
+            super(out);
+        }
+
+        public void writeString(String s) throws IOException {
+            writeBinary(s.getBytes());
+        }
+
+        public void writeBinary(byte[] buf) throws IOException {
+            writeInt(buf.length);
+            write(buf);
+        }
+
+        private void writeBigInt(BigInteger i) throws IOException {
+            writeBinary(i.toByteArray());
+        }
+    }
+
+    /**
+     * Gets RSA key fingerprint in the form of "f7:7a:42:76:79:e8:8a:1a:4a:32:0c:b3:f9:3b:53:d4"
+     */
+    public static String getFingerPrint(PublicKey key) throws GeneralSecurityException {
+        byte[] extractedBytes =  encode(key);
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest(extractedBytes);
+        String str = Hex.encodeHexString(digest);
+
+        // insert ";"
+        StringBuilder buf = new StringBuilder(digest.length*3);
+        for (int i=0; i<str.length(); i+=2) {
+            if (buf.length()>0) buf.append(':');
+            buf.append(str,i,i+2);
+        }
+
+        return buf.toString();
+    }
+
+    /**
+     * Encodes RSA/DSA public keys into the OpenSSH format.
+     */
+    public static byte[] encode(PublicKey key) throws GeneralSecurityException {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream2 out = new DataOutputStream2(baos);
+
+            if (key instanceof RSAPublicKey) {
+                RSAPublicKeySpec spec = KeyFactory.getInstance("RSA").getKeySpec(key, RSAPublicKeySpec.class);
+
+                out.writeString("ssh-rsa");
+                out.writeBigInt(spec.getPublicExponent());
+                out.writeBigInt(spec.getModulus());
+
+                return baos.toByteArray();
+            }
+
+            if (key instanceof DSAPublicKey) {
+                DSAPublicKeySpec dsaspec = KeyFactory.getInstance("DSA").getKeySpec(key, DSAPublicKeySpec.class);
+
+                out.writeString("ssh-dss");
+                out.writeBigInt(dsaspec.getP());
+                out.writeBigInt(dsaspec.getQ());
+                out.writeBigInt(dsaspec.getG());
+                out.writeBigInt(dsaspec.getY());
+
+                return baos.toByteArray();
+            }
+
+            throw new IllegalArgumentException("Unexpected key type: "+key);
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+
 }
